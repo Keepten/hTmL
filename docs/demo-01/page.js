026b5a97565d848a7b294ea61e6aa8e0cfdfd523
USP <ulrich.sprick@gmx.de>
2022-10-30 13:33:25 +0100

    Anleitungen hinzugefügt

---
+++ docs/demo-01/page.js
@@ -0,0 +1,15 @@
+//
+// page.js
+// Grundkode fuer alle Webseiten
+// 
+
+// --- Funktionsdefinitionen
+
+function initPage() {
+	// Initialisiert die Seite
+	console.log( "hello world!" );
+}
+
+// --- Funktionsaufrufe, start des "Programms"
+
+initPage();
